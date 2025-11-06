e6d53bef550d608576ebc328d552d9f76bd49fdd
Toanpitpit <he187337tavantoan@gmail.com>
2025-11-06 13:30:48 +0700

    Dessign UI Login v1.4

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import AuthenticationPage from "../pages/Authentication";
 import VerifyEmail from "../components/VerifyEmail";
+import ViewProfile from "../pages/ViewProfilePage";
 
 export default function SwitchRouters() {
+const user = localStorage.getItem("user");
   return (
     <Routes>
       <Route path="/auth" element={<AuthenticationPage />} />
       <Route path="/verify-email" element={<VerifyEmail />} />
+      <Route path="/profile" element={<ViewProfile id={user.id} />}/>
       <Route path="*" element={<Navigate to="/auth" replace />} />
     </Routes>
   );
