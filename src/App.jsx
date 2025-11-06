90fecfd4a6b5f3bef6a33b2cffbacde425db782b
Toanpitpit <he187337tavantoan@gmail.com>
2025-11-06 09:43:14 +0700

    update UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "./components/sidebar";
 import AuthenticationPage from "./pages/Authentication";
+import ProfileUser from "./components/profile";
+import ViewProfile from "./pages/ViewProfilePage";
 
 
 export default function App() {
   return ( 
-    <AuthenticationPage></AuthenticationPage>
+    // <ViewProfile id = {1}></ViewProfile>
+    // <AuthenticationPage></AuthenticationPage>
+    <Sidebar></Sidebar>
   )
 }
